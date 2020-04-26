500be83662ec96c69aa9e64c22b869316dd10a04
Ameya Lokare <lokare.ameya@gmail.com>
2020-04-26 01:13:55 -0700

    Simple echo server

--- app.go
+++ app.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
 	fmt.Printf("Hello egress proxy\n")
+	listener, err := net.Listen("tcp", "127.0.0.1:6080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			go echoConnection(conn)
+		}
+
+	}
+}
+
+func echoConnection(conn net.Conn) {
+	b := make([]byte, 20)
+	for {
+		n, err := conn.Read(b)
+		if err != nil {
+			fmt.Println(err)
+			conn.Close()
+			return
+		}
+		if n > 0 {
+			_, err := conn.Write(b[:n])
+			if err != nil {
+				fmt.Println(err)
+				conn.Close()
+				return
+			}
+		}
+	}
 }
