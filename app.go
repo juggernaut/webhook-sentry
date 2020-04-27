b41e46e057871db8ade74e44a59028ef7092f5f6
Ameya Lokare <lokare.ameya@gmail.com>
2020-04-26 18:17:09 -0700

    Extremely simple http -> http proxy working

--- app.go
+++ app.go
@@ -2,25 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 )
 
 func main() {
 	fmt.Printf("Hello egress proxy\n")
-	listener, err := net.Listen("tcp", "127.0.0.1:6080")
+	tr := &http.Transport{
+		IdleConnTimeout:   20000,
+		DisableKeepAlives: true,
+	}
+	client := http.Client{Transport: tr}
+	server := &http.Server{
+		Addr:           ":9090",
+		Handler:        MyHttpHandler{client: client},
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(server.ListenAndServe())
+}
+
+// some struct
+type MyHttpHandler struct {
+	client http.Client
+}
+
+func (m MyHttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Host is %s\n", r.Host)
+	log.Printf("URL is %s\n", r.URL.String())
+	//fmt.Fprintf(w, "Hello Go HTTP")
+	outboundRequest, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			go echoConnection(conn)
-		}
-
+	resp, err := m.client.Do(outboundRequest)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
+	resp.Write(w)
 }
 
 func echoConnection(conn net.Conn) {
