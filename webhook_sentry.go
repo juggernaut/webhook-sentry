16dadacc9fa8ee103f067e5f92fad58f8f4d5581
Ameya Lokare <lokare.ameya@gmail.com>
2022-12-28 21:02:25 -0800

    Bind command line flags to viper keys

--- webhook_sentry.go
+++ webhook_sentry.go
@@ -32,9 +32,40 @@ func main() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-	rootCmd.Flags().String("listener-address", "", "Address to listen on")
+	rootCmd.Flags().StringVar(&cfgFile, "config", "", "Path to config file")
+	rootCmd.Flags().String("listener-address", ":9090", "Address to listen on")
+	rootCmd.Flags().String("listener-type", "http", "Type of listener (http or https)")
+	rootCmd.Flags().String("listener-cert-file", "", "Path to certificate file (required only if listener is https)")
+	rootCmd.Flags().String("listener-key-file", "", "Path to key file (required only if listener is https)")
+	rootCmd.Flags().Duration("connect-timeout", time.Second*10, "TCP connect timeout")
+	rootCmd.Flags().Duration("connection-lifetime", time.Second*60, "TCP connection lifetime")
+	rootCmd.Flags().Duration("read-timeout", time.Second*10, "TCP connection read timeout")
+	rootCmd.Flags().Bool("insecure-skip-cert-verification", false, "Skip target certificate verification (WARNING: not for production use!)")
+	rootCmd.Flags().Bool("insecure-skip-cidr-deny-list", false, "Skip checking CIDR deny list (WARNING: not for production use!)")
+	rootCmd.Flags().Uint32("max-response-body-size", 1048576, "Maximum response body size (in bytes) over which the connection is automatically shut down")
+	rootCmd.Flags().String("access-log-type", "text", "Type of access log (text or json)")
+	rootCmd.Flags().String("access-log-file", "", "Path to access log file (default goes to stdout)")
+	rootCmd.Flags().String("proxy-log-type", "text", "Type of proxy log (text or json)")
+	rootCmd.Flags().String("proxy-log-file", "", "Path to proxy log file (default goes to stdout)")
+	rootCmd.Flags().String("metrics-address", ":2112", "Address to expose prometheus metrics on")
+	rootCmd.Flags().StringSlice("cidr-deny-list", nil, "List of CIDRs to be blocked (see docs for default)")
 
 	viper.BindPFlag("listener.address", rootCmd.Flags().Lookup("listener-address"))
+	viper.BindPFlag("listener.type", rootCmd.Flags().Lookup("listener-type"))
+	viper.BindPFlag("listener.certFile", rootCmd.Flags().Lookup("listener-cert-file"))
+	viper.BindPFlag("listener.keyFile", rootCmd.Flags().Lookup("listener-key-file"))
+	viper.BindPFlag("connectTimeout", rootCmd.Flags().Lookup("connect-timeout"))
+	viper.BindPFlag("connectionLifetime", rootCmd.Flags().Lookup("connection-lifetime"))
+	viper.BindPFlag("readTimeout", rootCmd.Flags().Lookup("read-timeout"))
+	viper.BindPFlag("insecureSkipCertVerification", rootCmd.Flags().Lookup("insecure-skip-cert-verification"))
+	viper.BindPFlag("insecureSkipCidrDenyList", rootCmd.Flags().Lookup("insecure-skip-cidr-deny-list"))
+	viper.BindPFlag("maxResponseBodySize", rootCmd.Flags().Lookup("max-response-body-size"))
+	viper.BindPFlag("accessLog.type", rootCmd.Flags().Lookup("access-log-type"))
+	viper.BindPFlag("accessLog.file", rootCmd.Flags().Lookup("access-log-file"))
+	viper.BindPFlag("proxyLog.type", rootCmd.Flags().Lookup("proxy-log-type"))
+	viper.BindPFlag("proxyLog.file", rootCmd.Flags().Lookup("proxy-log-file"))
+	viper.BindPFlag("metrics.address", rootCmd.Flags().Lookup("metrics-address"))
+	viper.BindPFlag("cidrDenyList", rootCmd.Flags().Lookup("cidr-deny-list"))
 
 	viper.SetDefault("cidrDenyList", []string{
 		"127.0.0.0/8",
@@ -173,8 +204,4 @@ func validCidr(cidr string) proxy.Cidr {
 		panic("Invalid CIDR " + cidr)
 	}
 	return proxy.Cidr(*ipNet)
-}
-
-func secondsDuration(key string) time.Duration {
-	return time.Second * time.Duration(viper.GetInt(key))
 }
